b94f2ac34d561cc663e986227bff0649534dfcd6
= <=>
2024-02-24 00:41:03 +0700

    berhasil post data arduino

--- router.js
+++ router.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get("/try", controller.try);
 router.post("/post", controller.postData);
-router.post("/newpost/:param1", controller.newPost);
+router.get("/newpost/:param1", controller.newPost);
 
 module.exports = router;
