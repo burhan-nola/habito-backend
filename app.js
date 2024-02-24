001ff8a12b1d0170b346d6a0729593da40f6578f
BURHAN FAUZI SALAM <63770755+burhanfauzisalam@users.noreply.github.com>
2024-02-24 16:48:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const deviceModel = require("./models/device.js");
+const deviceModel = require("./models/devices.js");
 
 require("dotenv").config();
 const router = require("./router");
