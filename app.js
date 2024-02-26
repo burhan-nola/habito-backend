7075598bc17ba2f34471224da3fc97c936743088
= <=>
2024-02-26 10:46:39 +0700

    remove port

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const deviceModel = require("./models/devices.js");
 
 require("dotenv").config();
 const router = require("./router");
 
 const app = express();
 
-const port = 5000;
+const port = process.env["PORT"];
 const mongoString = process.env["DATABASE_URL"];
 
 mongoose.connect(mongoString);
@@ -25,36 +23,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", router);
-
-app.get('/api/stream', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-
-  // Mengirim data secara berkala ke klien
-  const interval = setInterval(async () => {
-    const data = { message: 'Pesan baru' };
-    res.write(`data: ${JSON.stringify(data)}\n\n`);
-
-    // Simpan data ke MongoDB setiap kali mengirim pembaruan
-    try {
-      await deviceModel.findOneAndUpdate(
-{idDevice:"habito_001"},
-{$set:{status: false}},
-{new:true}
-);
-      console.log('Data berhasil disimpan ke MongoDB');
-    } catch (error) {
-      console.error('Gagal menyimpan data ke MongoDB:', error);
-    }
-  }, 10000);
-
-  // Tangani penutupan koneksi
-  req.on('close', () => {
-    clearInterval(interval);
-    res.end();
-  });
-});
 
 app.listen(port, () => {
   console.log(`Service is online on port ${port}`);
--- arduino/post data.ino
+++ arduino/post data.ino
@@ -23,18 +23,18 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP()); 
-  
+  regDevice();
 }
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
-    if(checkStatus() == false){
+    if(!checkStatus()){
       logs();
     }
   }
   delay(10000);
 }
 
-int logs() {
+void logs() {
     WiFiClientSecure client;
     client.setInsecure();
 
@@ -66,7 +66,6 @@ int logs() {
     } else {
         Serial.println("[HTTPS] Unable to connect");
     }
-    return 0;
 }
 
 bool checkStatus() {
@@ -107,3 +106,40 @@ bool checkStatus() {
     }
 }
 
+String regDevice(){
+    WiFiClientSecure client;
+    client.setInsecure();
+
+    HTTPClient https;
+    String idDevice = "habito_001";
+    String endpoint = "/register";
+    String query = "?id=";
+
+    String fullUrl = url + endpoint + query + idDevice;
+
+    Serial.print("Requesting: ");
+    Serial.println(fullUrl);
+
+    if (https.begin(client, fullUrl)) {
+        int httpCode = https.GET();
+        Serial.print("HTTP Response Code: ");
+        Serial.println(httpCode);
+        
+          DynamicJsonDocument doc(1024);
+          deserializeJson(doc, https.getString());
+          String message = doc["message"];
+          
+        if (httpCode > 0) {
+            Serial.println("Response Body: ");
+            Serial.println(message);
+        }
+        https.end();
+
+        return message;
+    } else {
+        Serial.println("[HTTPS] Unable to connect");
+
+        // Return a value to indicate an error, for example, -1
+        return "Unable to connect";
+    }
+}
