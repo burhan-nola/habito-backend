43aa6c33f4c63d68aa9ae5b555a15a8e44fab3bb
= <=>
2024-02-27 10:32:57 +0700

    add middleware to auto offline

--- router.js
+++ router.js
@@ -6,9 +6,9 @@ const router = express.Router();
 
 router.get("/try", controller.try);
 // router.get("/newpost", controller.newPost);
-router.get("/logs", middleware, controller.logs);
+router.get("/logs", controller.logs);
 router.get("/offline", controller.offline);
 router.get("/register", controller.register);
-router.get("/cek", controller.cekStatus);
+router.get("/cek", middleware, controller.cekStatus);
 
 module.exports = router;
