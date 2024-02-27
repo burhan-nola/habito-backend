8619bd559a7b2e7651af000ebbaa6485039bd8e8
= <=>
2024-02-27 09:07:44 +0700

    add cronjob

--- cronjob/cekOffline.js
+++ cronjob/cekOffline.js
@@ -1,9 +1,12 @@
+const express = require("express");
+const app = express();
+
 const { toLocalDate } = require("./functions/toLocalDate.js");
 
 const deviceModel = require("./models/devices.js");
 const logsModel = require("./models/logs.js");
 
-const cekOffline = async () => {
+app.get("/cek-offline", async (req, res) => {
   const localDate = toLocalDate();
   const updateData = await deviceModel.findOneAndUpdate(
     { idDevice: "habito_001" },
@@ -18,4 +21,7 @@ const cekOffline = async () => {
   const logStatus = new logsModel(saveUpdate);
   await logStatus.save();
   console.log("semua device offline");
-};
+  res.json({ message: "cek semua device!" });
+});
+
+module.exports = app;
