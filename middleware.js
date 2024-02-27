1832933eb2b6b3b2904eddead71f3ea46fc8d08c
= <=>
2024-02-27 10:54:59 +0700

    add middleware to auto offline

--- middleware.js
+++ middleware.js
@@ -1,13 +1,22 @@
 const deviceModel = require("./models/devices.js");
+const { toLocalDate } = require("./functions/toLocalDate.js");
 
 const cekStatus = (req, res, next) => {
   try {
     const offline = async () => {
+      const localDate = toLocalDate();
       const updateData = await deviceModel.findOneAndUpdate(
         { idDevice: "habito_001" },
         { $set: { status: false } },
         { new: true }
       );
+      const saveUpdate = {
+        idDevice: "habito_001",
+        status: false,
+        date: localDate,
+      };
+      const logStatus = new logsModel(saveUpdate);
+      await logStatus.save();
     };
     setTimeout(() => {
       offline();
