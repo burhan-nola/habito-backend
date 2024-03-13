6283e26c02b073bb5b8f6e41734e7f09949e4feb
= <=>
2024-03-13 13:00:49 +0700

    fixed bug change light status

--- controllers/lightStatus.js
+++ controllers/lightStatus.js
@@ -10,7 +10,9 @@ exports.light = async (req, res) => {
 
     const data = await deviceModel.findOne({ idDevice: req.query.id });
     const lightData = data.light[light];
-    const lastStatus = lightData[lightData.length - 1];
+    const lastStatus =
+      lightData.length > 0 ? lightData[lightData.length - 1] : [];
+
     if (!lastStatus.status) {
       const updateData = {
         status: true,
@@ -23,7 +25,7 @@ exports.light = async (req, res) => {
         .status(201)
         .json({ message: `${light} light is on`, data: updateData });
     }
-    res.status(200).json({ message: "Task is done" });
+    res.status(200).json(lastStatus);
   } catch (error) {
     res.status(500).json(error);
   }
